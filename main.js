2f56a9416050f4ea8cfcf4418387c35973cd3a8a
Mikolaj Korzeniowski <korzeniowskimikolaj@gmail.com>
2020-05-21 15:55:03 +0200

    added bem to html

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-console.log('hello')
+console.log("hello");
 
-let hello_message = "hello siemanko"
+let hello_message = "hello siemanko";
 const name = "Maciek";
 const age = 32;
 
-console.log(`Chciałbym wam powiedzieć ${hello_message}`)
+console.log(`Chciałbym wam powiedzieć ${hello_message}`);
